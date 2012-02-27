1d773e0b2923f2480e1d5ebeb3e01197645406a1
Sam Waterhouse <samwa_oz@yahoo.co.uk>
2012-02-27 14:55:57 +1100

    adding custom config section

--- ProgrammableWebAPI.Test/TestBase.cs
+++ ProgrammableWebAPI.Test/TestBase.cs
@@ -5,18 +5,24 @@ using System.Text;
 using NUnit.Framework;
 using System.Configuration;
 using ProgrammableWebAPI.Managers;
+using ProgrammableWebAPI.ConfigHandlers;
 
 namespace ProgrammableWebAPI.Test
 {
     public class TestBase
     {
         internal PWContext context;
-        private static string hostName = ConfigurationManager.AppSettings["ProgrammableWeb.HostName"];
-        private static string key = ConfigurationManager.AppSettings["ProgrammableWeb.Key"];
+        PWWebAPIConfigurationSection config = (PWWebAPIConfigurationSection)ConfigurationManager.GetSection("PWAPIGroup/PWAPI");
+
+        private static string hostName;
+        private static string key;
 
         [SetUp]
         public void SetupTest()
         {
+            hostName = config.HostName.Value;
+            key = config.Key.Value;
+
             context = new PWContext(hostName, key);
         }
     }
---
+++ ProgrammableWebAPI/ConfigHandlers/PWWebAPIConfigurationSection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace ProgrammableWebAPI.ConfigHandlers
+{
+    public class PWWebAPIConfigurationSection : ConfigurationSection
+    {
+
+        // Create a "hostname" element.
+        [ConfigurationProperty("hostname")]
+        public HostNameElement HostName
+        {
+            get
+            {
+                return (HostNameElement)this["hostname"];
+            }
+            set
+            { this["hostname"] = value; }
+        }
+
+        // Create a "key" element.
+        [ConfigurationProperty("key")]
+        public KeyElement Key
+        {
+            get
+            {
+                return (KeyElement)this["key"];
+            }
+            set
+            { this["key"] = value; }
+        }
+    }
+
+    public class HostNameElement : ConfigurationElement
+    {
+        [ConfigurationProperty("value", DefaultValue = "test", IsRequired = true)]
+        [StringValidator(InvalidCharacters = "~!@#$%^&*()[]{};'\"|\\", MinLength = 2, MaxLength = 600)]
+        public String Value
+        {
+            get
+            {
+                return (String)this["value"];
+            }
+            set
+            {
+                this["value"] = value;
+            }
+        }
+
+
+    }
+
+    public class KeyElement : ConfigurationElement
+    {
+        [ConfigurationProperty("value", DefaultValue = "XXXX", IsRequired = true)]
+        [StringValidator(InvalidCharacters = "'\"\\", MinLength = 1, MaxLength = 60)]
+        public String Value
+        {
+            get
+            {
+                return (String)this["value"];
+            }
+            set
+            {
+                this["value"] = value;
+            }
+        }
+    }
+}
